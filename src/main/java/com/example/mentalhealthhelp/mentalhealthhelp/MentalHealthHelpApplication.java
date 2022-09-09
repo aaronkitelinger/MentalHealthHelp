362e7f4ed7dd3c517340dd88c039672549440625
Aaron Kitelinger <kiteliad@mail.uc.edu>
2022-09-09 17:49:30 -0400

    Initial commit

---
+++ src/main/java/com/example/mentalhealthhelp/mentalhealthhelp/MentalHealthHelpApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentalhealthhelp.mentalhealthhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentalhealthhelp/mentalhealthhelp/MentalHealthHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentalhealthhelp.mentalhealthhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
