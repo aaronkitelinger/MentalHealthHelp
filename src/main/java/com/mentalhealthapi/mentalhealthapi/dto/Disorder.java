ab2458459e141d2c15ed0d520f3990de723bc3b2
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-09-24 07:56:01 -0400

    Added a dto dir

---
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/dto/Disorder.java
@@ -0,0 +1,9 @@
+package com.mentalhealthapi.mentalhealthapi.dto;
+
+public class Disorder {
+    private int id;
+    private String name;
+    private String description;
+    private String dsm5Page;
+    private float populationPercent;
+}
