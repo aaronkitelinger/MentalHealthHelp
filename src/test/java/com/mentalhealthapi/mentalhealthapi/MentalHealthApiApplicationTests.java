9c5db53ee9df71a89598ce8b0c2bbf4568a2b878
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-09-24 07:40:34 -0400

    Project re-setup

---
+++ src/test/java/com/mentalhealthapi/mentalhealthapi/MentalHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentalhealthapi.mentalhealthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
