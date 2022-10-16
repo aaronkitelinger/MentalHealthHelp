7c0f429d6da2412e70a1e7dcd7ac2a5ec058e02f
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-10-16 18:07:16 -0400

    Wrote tests for TDD

--- src/test/java/com/mentalhealthapi/mentalhealthapi/MentalHealthControllerTest.java
+++ src/test/java/com/mentalhealthapi/mentalhealthapi/MentalHealthControllerTest.java
@@ -1,6 +1,8 @@
 package com.mentalhealthapi.mentalhealthapi;
 
+import com.mentalhealthapi.mentalhealthapi.dto.Blog;
 import com.mentalhealthapi.mentalhealthapi.dto.Disorder;
+import com.mentalhealthapi.mentalhealthapi.service.IBlogService;
 import com.mentalhealthapi.mentalhealthapi.service.IDisorderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,42 @@ public class MentalHealthControllerTest {
     MockMvc mockMvc;
     @MockBean
     IDisorderService disorderService;
+
+    @MockBean
+    IBlogService blogService;
+
+    @Test
+    void shouldReturnBlogViewWirthPrefilledData() throws Exception {
+
+        Blog blog1 = new Blog();
+        blog1.setDisorderName("Depression");
+        blog1.setTitle("Day 1");
+        blog1.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
+
+        Blog blog2 = new Blog();
+        blog2.setDisorderName("Bipolar Disorder");
+        blog2.setTitle("Day 1");
+        blog2.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
+
+        Blog blog3 = new Blog();
+        blog3.setDisorderName("RAD");
+        blog3.setTitle("Day 1");
+        blog3.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
+
+        List<Blog> blogs = new ArrayList<>();
+        blogs.add(blog1);
+        blogs.add(blog2);
+        blogs.add(blog3);
+
+        when(blogService.fetchAll()).thenReturn(blogs);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/blog"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("blog"))
+                .andExpect(model().attributeExists("blogs"));
+
+    }
 
     @Test
     void shouldReturnBlogView() throws Exception {
