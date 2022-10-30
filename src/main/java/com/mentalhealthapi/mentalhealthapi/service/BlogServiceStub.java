da440f20134ec33b48cfaee0ddda148daff675a0
Danae Brenner <danae.brenner@westernsouthernlife.com>
2022-10-30 18:47:38 -0400

    Danae Brenner Code Review 2

--- src/main/java/com/mentalhealthapi/mentalhealthapi/service/BlogServiceStub.java
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/service/BlogServiceStub.java
@@ -6,7 +6,7 @@ import com.mentalhealthapi.mentalhealthapi.dto.Blog;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlogServiceStub implements IBlogService {
+public class BlogServiceStub {
 
     private IBlogDatabaseAccess blogDAO;
 
@@ -20,7 +20,7 @@ public class BlogServiceStub implements IBlogService {
      *
      * @return a list of blogs
      */
-    @Override
+
     public List<Blog> fetchAll() {
         Blog blog1 = new Blog();
         blog1.setDisorderName("Depression");
@@ -50,7 +50,7 @@ public class BlogServiceStub implements IBlogService {
      *
      * @param id
      */
-    @Override
+
     public void deleteById(int id) {
         System.out.println("Blog deleted. Blog Id: " + id);
     }
@@ -61,7 +61,7 @@ public class BlogServiceStub implements IBlogService {
      * @param blog1
      * @return
      */
-    @Override
+
     public Blog save(Blog blog1) throws Exception {
         return blogDAO.save(blog1);
     }
--- src/test/java/com/mentalhealthapi/mentalhealthapi/MentalHealthControllerTest.java
+++ src/test/java/com/mentalhealthapi/mentalhealthapi/MentalHealthControllerTest.java
@@ -1,22 +1,11 @@
 package com.mentalhealthapi.mentalhealthapi;
-import com.mentalhealthapi.mentalhealthapi.dao.IBlogDatabaseAccess;
-import com.mentalhealthapi.mentalhealthapi.dto.Blog;
-import com.mentalhealthapi.mentalhealthapi.dto.Disorder;
-import com.mentalhealthapi.mentalhealthapi.service.BlogServiceStub;
-import com.mentalhealthapi.mentalhealthapi.service.IBlogService;
-import com.mentalhealthapi.mentalhealthapi.service.IDisorderService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MentalHealthController.class)
@@ -24,37 +13,6 @@ public class MentalHealthControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-    @MockBean
-    IDisorderService disorderService;
-    @MockBean
-    IBlogService blogService;
-    @MockBean
-    IBlogDatabaseAccess blogDAO;
-
-    Blog mockBlog = createMockBlog();
-
-    private Blog createMockBlog() {
-        Blog mockBlog = new Blog();
-        mockBlog.setDisorderName("TEST");
-        mockBlog.setTitle("TITLE HERE");
-        mockBlog.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
-        return mockBlog;
-    }
-
-    @Test
-    void shouldReturnBlogViewWithPrefilledData() throws Exception {
-        List<Blog> blogs = new ArrayList<>();
-        blogs.add(mockBlog);
-
-        when(blogService.fetchAll()).thenReturn(blogs);
-
-        mockMvc
-                .perform(MockMvcRequestBuilders.get("/blog"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("blog"))
-                .andExpect(model().attributeExists("blogs"));
-
-    }
 
     @Test
     void shouldReturnBlogView() throws Exception {
@@ -75,14 +33,6 @@ public class MentalHealthControllerTest {
     @Test
     void shouldReturnDisorderViewWithPrefilledData() throws Exception {
 
-        Disorder depression = new Disorder();
-        depression.setName("Depression");
-        depression.setDescription("A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
-
-        List<Disorder> disorders = new ArrayList<>();
-        disorders.add(depression);
-
-        when(disorderService.fetchAllDisorders()).thenReturn(disorders);
 
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/disorders"))
@@ -91,24 +41,5 @@ public class MentalHealthControllerTest {
                 .andExpect(model().attributeExists("disorders"));
     }
 
-    @Test
-    void shouldCreateBlogPostByUser() throws Exception {
-        when(blogDAO.save(mockBlog)).thenReturn(mockBlog);
-        blogService = new BlogServiceStub(blogDAO);
 
-//        mockMvc
-//                .perform(MockMvcRequestBuilders.post("/blog"))
-//                .andExpect(status().isOk());
-
-        Blog newBlog = blogService.save(mockBlog);
-        assertEquals(mockBlog, newBlog);
-        verify(blogDAO, atLeastOnce()).save(mockBlog);
-    }
-
-    @Test
-    void shouldDeleteBlogPostByUser() throws Exception {
-        mockMvc
-                .perform(MockMvcRequestBuilders.delete("/blog/1/"))
-                .andExpect(status().isOk());
-    }
 }
