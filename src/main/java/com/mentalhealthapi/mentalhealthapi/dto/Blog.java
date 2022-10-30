d5854c4a5161fdd6999b9fb73b585826f1f516d8
mrmaxmax1423 <maximilianods@ymail.com>
2022-10-30 19:19:19 -0400

    Created DTO getters and setters
    Created getters and setters for Blog and Disorder DTOs, and made the variable dsm5 an int instead of a string

--- src/main/java/com/mentalhealthapi/mentalhealthapi/dto/Blog.java
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/dto/Blog.java
@@ -7,4 +7,36 @@ public @Data class Blog {
     private String disorderName;
     private String title;
     private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDisorderName() {
+        return disorderName;
+    }
+
+    public void setDisorderName(String disorderName) {
+        this.disorderName = disorderName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
--- src/main/java/com/mentalhealthapi/mentalhealthapi/dto/Disorder.java
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/dto/Disorder.java
@@ -6,6 +6,46 @@ public @Data class Disorder {
     private int id;
     private String name;
     private String description;
-    private String dsm5Page;
+    private int dsm5Page;
     private float populationPercent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDsm5Page() {
+        return dsm5Page;
+    }
+
+    public void setDsm5Page(int dsm5Page) {
+        this.dsm5Page = dsm5Page;
+    }
+
+    public float getPopulationPercent() {
+        return populationPercent;
+    }
+
+    public void setPopulationPercent(float populationPercent) {
+        this.populationPercent = populationPercent;
+    }
 }
