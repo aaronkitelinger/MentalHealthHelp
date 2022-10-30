3b4f278dead0dc5226bf3bf4cf775de2f82ec8d9
mrmaxmax1423 <maximilianods@ymail.com>
2022-10-30 19:27:32 -0400

    Fixed Javadoc for the Service Stubs
    Changed the documentation comments to show exactly what the service stubs were doing

--- src/main/java/com/mentalhealthapi/mentalhealthapi/service/BlogServiceStub.java
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/service/BlogServiceStub.java
@@ -15,33 +15,35 @@ public class BlogServiceStub {
         this.blogDAO = blogDAO;
     }
 
-    /**
-     * Fetch all blogs - regardless the user
-     *
-     * @return a list of blogs
-     */
-
     public List<Blog> fetchAll() {
+        //creates a new Blog entry named blog1
+        //sets name title and body for blog1
         Blog blog1 = new Blog();
         blog1.setDisorderName("Depression");
         blog1.setTitle("Day 1");
         blog1.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
 
+        //creates a new Blog entry named blog2
+        //sets name title and body for blog2
         Blog blog2 = new Blog();
         blog2.setDisorderName("Bipolar Disorder");
         blog2.setTitle("Day 1");
         blog2.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
 
+        //creates a new Blog entry named blog3
+        //sets name title and body for blog3
         Blog blog3 = new Blog();
         blog3.setDisorderName("RAD");
         blog3.setTitle("Day 1");
         blog3.setBody("This is the body text of this blog. Example Text: A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
 
+        //creates a list with the populated blog entries
         List<Blog> blogs = new ArrayList<>();
         blogs.add(blog1);
         blogs.add(blog2);
         blogs.add(blog3);
 
+        //returns lost of blog entries to be displayed on the blog page
         return blogs;
     }
 
--- src/main/java/com/mentalhealthapi/mentalhealthapi/service/DisorderServiceStub.java
+++ src/main/java/com/mentalhealthapi/mentalhealthapi/service/DisorderServiceStub.java
@@ -8,26 +8,26 @@ import java.util.List;
 
 @Component
 public class DisorderServiceStub implements IDisorderService {
-
-    /**
-     * Fetch all disorders
-     *
-     * @return
-     */
     @Override
     public List<Disorder> fetchAllDisorders() {
+        //creates a new Disorder named depression
+        //sets name and description for depression
         Disorder depression = new Disorder();
         depression.setName("Depression");
         depression.setDescription("A group of conditions associated with the elevation or lowering of a person's mood, such as depression or bipolar disorder.");
 
+        //creates a new Disorder named bipolar
+        //sets name and description for bipolar
         Disorder bipolar = new Disorder();
         bipolar.setName("Bipolar Disorder");
         bipolar.setDescription("Someone with bipolar disorder, which is also sometimes called \"manic depression,\" has mood episodes that range from extremes of high energy with an \"up\" mood to low \"depressive\" periods.");
 
+        //creates a list with the populated disorders
         List<Disorder> disorders = new ArrayList<>();
         disorders.add(depression);
         disorders.add(bipolar);
 
+        //return list of disorders to display on the disorders page
         return disorders;
     }
 }
